1d3b74d439cc47888781ac6d5a17d2a6832d932c
Athoy <athoykanti.roy1612@gmail.com>
2024-01-01 20:52:01 +0600

    PharmacyManagementSystem

---
+++ Desktop/Pharmacy_Management_System/Pharmacy_Management_System/App layer/DAL/DataAccessFactory.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Interface;
+using DAL.Models.Entities;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IProfile<Pharmacist, int, bool> PharmacistData()
+        {
+            return new PharmacistRepo();
+        }
+    }
+}
---
+++ Desktop/Pharmacy_Management_System/Pharmacy_Management_System/App layer/DAL/Repos/PharmacistRepo.cs
@@ -0,0 +1,43 @@
+﻿using DAL.Interface;
+using DAL.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    public class PharmacistRepo : Repo, IProfile<Pharmacist, int, bool>
+    {
+        public bool Create(Pharmacist obj)
+        {
+            db.Pharmacists.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var pharmacist = Get(id);
+            db.Pharmacists.Remove(pharmacist);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<Pharmacist> Get()
+        {
+            return db.Pharmacists.ToList();
+        }
+
+        public Pharmacist Get(int id)
+        {
+            return db.Pharmacists.Find(id);
+        }
+
+        public bool Update(Pharmacist obj)
+        {
+            var pharmacist = Get(obj.Id);
+            db.Entry(pharmacist).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
